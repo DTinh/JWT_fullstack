4bb728ee6ab34dd7aac013d9f30d0e59c55aa033
DTinh <duytinh10833@gmail.com>
2024-11-25 16:19:40 +0700

    react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './components/Navigation/Nav';
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import Login from '../src/components/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app-container'>
+        {/* <Nav /> */}
+        <Switch>
+          <Route exact path="/new">
+            new
+          </Route>
+          <Route exact path="/about">
+            about
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            home
+          </Route>
+          <Route path="*" exact>
+            404 not found
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
 export default App;
+
---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,13 @@
+
+
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            hello login
+            <button>test</button>
+        </div>
+    )
+}
+
+export default Login;
